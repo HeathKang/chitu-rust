cc512e1ff80e32a75a158146faf3f0bb4734d7df
heathkang <heathkang>
2018-06-19 22:19:21 +0800

    first init

---
+++ README.md
@@ -0,0 +1,2 @@
+## chitu
+A data transmitter from redis to influxdb. Often used with Doctopus.
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+#[macro_use]
+extern crate influx_db_client;
+extern crate redis;
+
+
+fn main() {
+    println!("Hello, world!");
+}
