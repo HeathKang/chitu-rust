fd9be34a88587a31d55079866eb716fdcb73e479
heathkang <heath.kang@foxmail.com>
2018-06-20 22:47:43 +0800

    instgall vs 2017

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,15 @@ extern crate redis;
 
 fn main() {
     println!("Hello, world!");
+    fetch_an_integer();
+}
+
+fn fetch_an_integer() -> redis::RedisResult<isize>{
+    let client = try!(redis::Client::open("redis://127.0.0.1/"));
+    let con = try!(client.get_connection());
+
+    let _ :() = try!(con.set("my_key", 42));
+
+    con.get("my_key")
+
 }
